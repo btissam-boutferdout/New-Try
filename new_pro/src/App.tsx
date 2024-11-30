97147392bf7f223fff0a5617123895d6bdf858aa
btissam-boutferdout <btissam.boutferdout@icloud.com>
2024-11-30 22:27:11 +0100

    first commit

---
+++ new_pro/src/App.tsx
@@ -0,0 +1,15 @@
+import Grid from "./components/Grid";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <main>
+        <Navbar />;
+        <Grid />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ new_pro/src/components/Grid.tsx
@@ -0,0 +1,21 @@
+const Grid = () => {
+  return (
+    <div>
+      <div className="container text-center p-5">
+        <div className="row">
+          <div className="col-12 col-md-4">
+            <img src="img/img1.jpg" alt="" className="m-3" />
+          </div>
+          <div className="col-12 col-md-4">
+            <img src="img/img1.jpg" alt="" className="m-3" />
+          </div>
+          <div className="col-12 col-md-4">
+            <img src="img/img1.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Grid;
